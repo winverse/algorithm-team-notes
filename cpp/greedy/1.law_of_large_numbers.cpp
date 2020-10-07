f5392dfae369d3bf945f498e8d325841f6c914df
sangHyun <public.winverse@gmail.com>
2020-10-07 20:47:21 +0900

    [Cpp] law of large numbers

---
+++ cpp/greedy/1.law_of_large_numbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> v;
+
+int main(void) 
+{
+  int n, m, k;
+  cin >> n >> m >> k;
+
+  for(int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    v.push_back(x);
+  }
+
+  sort(v.begin(), v.end());
+
+  int first = v[n - 1];
+  int second = v[n - 2];
+
+  int count = (m / (k + 1)) * k;
+  count += m % (k + 1);
+
+  int result = 0;
+  result += count * first;
+  result += (m - count) * second;
+
+  cout << result << '\n';
+}
+
